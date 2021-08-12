5bb6e69db7ca8c1f7b15fb65d95751755722df0a
aweoluwatobi <aweoluwatobi26@gmail.com>
2021-08-12 01:25:07 +0100

    allow overlay to be hidden until show modal button is clicked

--- script.js
+++ script.js
@@ -3,19 +3,23 @@
 const showModal = document.querySelectorAll('.show-modal');
 const modal = document.querySelector('.modal');
 const closeModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
 
 for (let i = 0; i < showModal.length; i++) {
   showModal[i].addEventListener('click', function () {
     modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
   });
 }
 
 closeModal.addEventListener('click', function () {
   modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 });
 
 document.querySelector('body').addEventListener('keydown', function (event) {
   if (event.key === 'Escape') {
     modal.classList.add('hidden');
+    overlay.classList.add('hidden');
   }
 });
