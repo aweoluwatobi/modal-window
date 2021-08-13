f4a49295b57185d90034a2d9be93ab380d103903
aweoluwatobi <aweoluwatobi26@gmail.com>
2021-08-13 16:57:22 +0100

    Set code to close modal  when overlay is clicked

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 'use strict';
 
-const showModal = document.querySelectorAll('.show-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
 const modal = document.querySelector('.modal');
-const closeModal = document.querySelector('.close-modal');
+const btnCloseModal = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
 
-for (let i = 0; i < showModal.length; i++) {
-  showModal[i].addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-}
+const showModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
-closeModal.addEventListener('click', function () {
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-});
+};
 
-document.querySelector('body').addEventListener('keydown', function (event) {
-  if (event.key === 'Escape') {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener('click', showModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.querySelector('body').addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    closeModal();
   }
 });
